2c471f05cbe89b08d99d30174ca772576df3b071
Brian Pruitt-Goddard <bpruitt-goddard@github>
2025-04-04 21:47:25 -0700

    feat: add baseline

--- keyboards/crkbd/rev1/keymaps/bpg/rules.mk
+++
@@ -1,6 +0,0 @@
-# OLED_ENABLE = yes
-# OLED_DRIVER = SSD1306
-# MOUSEKEY_ENABLE     = no     # Mouse keys
-RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
-VIA_ENABLE          = yes         # Enable VIA
-LTO_ENABLE          = yes
--- keyboards/crkbd/rev1/keymaps/bpgBaseline/config.h
+++ keyboards/crkbd/rev1/keymaps/bpgBaseline/config.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 //#define USE_MATRIX_I2C
-#define MASTER_RIGHT
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
