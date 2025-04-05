e33bfa21b3533579d168c33886da64ee63fe74f4
Brian Pruitt-Goddard <bpruitt-goddard@github>
2025-04-04 21:22:07 -0700

    temp: try old ergohaven qmk board

--- keyboards/crkbd/rev1/keymaps/bpg/config.h
+++ keyboards/crkbd/rev1/keymaps/bpg/config.h
@@ -1,19 +1,4 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -24,6 +9,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
+#define MASTER_RIGHT
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
@@ -40,4 +26,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+
 #endif
--- keyboards/crkbd/rev1/keymaps/bpg/rules.mk
+++ keyboards/crkbd/rev1/keymaps/bpg/rules.mk
@@ -1,5 +1,6 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-# # OLED_ENABLE     = yes
-# OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+# OLED_ENABLE = yes
+# OLED_DRIVER = SSD1306
+# MOUSEKEY_ENABLE     = no     # Mouse keys
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+LTO_ENABLE          = yes
